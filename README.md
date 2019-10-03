6a1996c48fcd28fd3ae754f9cf482b3a45907ace
Erin Koen <erin.koen@gmail.com>
2019-10-03 13:11:11 -0400

    Write TSV successfully, handle edge cases where no such fund exists, or the fund is a holding company for other funds.

--- README.md
+++ README.md
@@ -15,26 +15,28 @@ TODO: Fill out this long description.
 - [License](#license)
 
 ## Background
+The take home challenge for the Plaid Software Engineer - Crawler role. 
+
+This was a fun challenge as I'd never worked with Selenium or Beautiful Soup before. I've written a lot of code in Python, but it's either been for the computer science theory portion of Lambda School where we optimized algorithms and built data structures, or it's been writing APIs in Django and Flask. It was super fun to learn a couple new packages by exploring the docs and trying to solve what was an intricate problem. 
+
+My proudest moment in this code is handling the complexity on the 13F page. The naming conventions for the acutal XML file that contains a fund's holdings seem to be fairly lax. THat said, I noticed that the Type column of the table where the link was held was constant - `INFORMATION TABLE`. I used beautiful soup to break each row in the table into an array, found the cell that held `INFORMATION TABLE` and worked backwards from there. This is an obvious assumption on my part, but one I'm happy to make because I dug around a bit and couldn't find an instance where this wasn't the case. 
+
+The Black Rock edge case was also interesting. In this instance, Black Rock is the holding company for a bunch of other funds and money managers, so they leave reporting up to their subsidiaries. I decided just to log that error message to the console, rather than printing a blank TSV file, thinking that as a user I'd rather the immediate feedback.  
 
 ## Install
 
-```
-```
+You've successfully unzipped the file if you're reading this text. The next step will be to install the dependencies. Navigate to the directory in your terminal and launch your favorite virtual environment. I happen to prefer pipenv. `Pipenv install` will load all the dependencies (listed in the `Pipefile`) required by the program. They're also listed in the `requirements.txt` in this directory.
+
+This program was built in a code editor and has been run through the terminal. Installation instructions are written accordingly.
 
 ## Usage
 
-```
-```
+After navigating to the ERIN_KOEN directory in your terminal, enter `python sec_scraper.py CIK_or_ticker_ofyourchoosing`. The program will run and let you know that a TSV file has been created when it's finished. The TSV file will be named `CIK_todaysdate` where CIK is the ticker or number you entered. 
 
 ## Maintainers
 
 [@erin-koen](https://github.com/erin-koen)
 
-## Contributing
-
-
-
-Small note: If editing the README, please conform to the [standard-readme](https://github.com/RichardLitt/standard-readme) specification.
 
 ## License
 
--- sec_scraper.py
+++ sec_scraper.py
@@ -1,73 +1,123 @@
-import requests
-import lxml
-import time
 import csv
-import xmlschema
-from pprint import pprint
+import datetime
+import lxml
+import sys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
-
 from bs4 import BeautifulSoup
 
 
-options = webdriver.ChromeOptions()
-options.add_argument("--headless")
-driver = webdriver.Chrome(options=options)
+def generate_tsv_from_13f(cik):
+    identifier_string = cik if type(cik) is 'string' else str(cik)
+    today = datetime.datetime.now()
+    modified_today = f'{today.year}_{today.month}_{today.day}'
 
-# interpolate search URL
-driver.get(
-    'https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=0001037766&type=13f')
-# find the first 13f docs button and click it
-ele = driver.find_element_by_xpath("//a[@id='documentsbutton']").click()
+    # instantiate selenium driver instance
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(options=options)
 
-# wait for client html call to resolve to DOM
-table_div = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.CLASS_NAME, "tableFile")))
+    # interpolate search URL
+    driver.get(
+        f'https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK={identifier_string}&type=13f')
+
+    try:
+        ele = driver.find_element_by_xpath(
+            "//a[@id='documentsbutton']").click()
+
+    except NoSuchElementException as ex:
+        print("That's not a valid mutual fund ticker. It may be either an ETF or a single name equity symbol.")
+        return
+
+    # find the first 13f docs button and click it
+
+    # wait for client html call to resolve to DOM
+    table_div = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "tableFile")))
+
+    # use beautiful soup to find correct link
+    source_overview = driver.page_source
+    soup = BeautifulSoup(source_overview, "html.parser")
+
+    # there are two tables on this page - we're looking for the second
+    table = soup.find_all("table")[-1]
+
+    # get all the rows in the table except for the headers
+    table_rows = table.find_all("tr")[1:]
+
+    documents = []
+
+    # loop through the rows and generate an array from each cell,
+    for tr in table_rows:
+        td = tr.find_all('td')
+        row = [i.text for i in td]
+        # push it onto the empty array for later
+        documents.append(row)
+
+    # find the XML link using list comprehension
+    link = [document[2]
+            for document in documents if
+            # assumption here that all 13fs will be labled as information table
+            # in the ones I checked, this was true but... still an assumption
+            document[3] == 'INFORMATION TABLE' and
+            document[2][-3:] == 'xml']
+
+    # handle the Black Rock edge case - they're an umbrella for a bunch
+    # of other Black Rock mutual funds. Those funds report individually.
+    if len(link) is not 1:
+        # with open(f'{cik}.tsv', 'wt') as output:
+
+        print('This institution either leaves the reporting of their holdings to their subsidiaries, or they report in a manner that has not yet been codified.')
+        return
+
+    # selenium to locate the link to the xml filing
+    xml_link = driver.find_element_by_link_text(link[0])
+    xml_link.click()
+
+    # wait for client html request to resolve
+    information_table = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.TAG_NAME, "informationTable")))
+
+    # generate HTML for beautiful soup to parse
+    source_overview = driver.page_source
+
+    # make the soup
+    xml_soup = BeautifulSoup(source_overview, features='lxml')
+
+    # parse the soup
+    body = xml_soup.body
+    table = body.div
+    positions = table.find_all('infotable')
+
+    output_array = []
+
+    #  generate an object for each position and push it onto the empty array
+    for position in positions:
+        position_object = {}
+        position_object['Name'] = position.nameofissuer.get_text()
+        position_object['Share Class'] = position.titleofclass.get_text()
+        position_object['Number of Shares'] = position.sshprnamt.get_text()
+        output_array.append(position_object)
+
+    # write the tsv
+    with open(f'{cik}_{modified_today}.tsv', 'wt') as output:
+        tsv_writer = csv.writer(output, delimiter='\t')
+        tsv_writer.writerow(['Company', 'Share Class', 'Number of Shares'])
+        for position in output_array:
+            tsv_writer.writerow([
+                position['Name'],
+                position['Share Class'],
+                position['Number of Shares']])
+
+    print('TSV file written succesfully.')
+    return
 
 
-# use beautiful soup to find correct link
-source_overview = driver.page_source
-soup = BeautifulSoup(source_overview, "html.parser")
-
-table = soup.find_all("table")[-1]
-table_rows = table.find_all("tr")[1:]
-documents = []
-
-for tr in table_rows:
-    td = tr.find_all('td')
-    row = [i.text for i in td]
-    documents.append(row)
-
-link = [document[2]
-        for document in documents if
-        document[3] == 'INFORMATION TABLE' and
-        document[2][-3:] == 'xml']
-if len(link) is not 1:
-    print("This institution either leaves the reporting of their holdings to their subsidiaries, or they report in a manner that has not yet been codified")
-
-xml_link = driver.find_element_by_link_text(link[0])
-xml_link.click()
-# print('before')
-time.sleep(10)
-# driver.implicitly_wait(20)
-# print('after')
-source_overview = driver.page_source
-
-
-# print('source_overview', source_overview)
-xml_soup = BeautifulSoup(source_overview, features='lxml')
-body = xml_soup.find_all("body")
-print(body)
-# # request = requests.get(ele_link)
-# print(request.text)
-
-# def cik_search(cik):
-#     request = requests.get(
-#         f'https://www.sec.gov/cgi-bin/browse-edgar?CIK={cik}$type=13f')
-#     return request.text
-
-
-# print(cik_search('VOYA'))
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Please pass one and only one CIK or ticker to the function")
+    else:
+        generate_tsv_from_13f(sys.argv[1])
